8b0c0c218126c7cf0d4f91b0bd938752b228c4ee
Ronald Chacon <ronald.chacon87@gmail.com>
2015-05-22 11:35:46 -0700

    Add form to request users email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   helper_method :user_signed_in?
-  helper_method :current_user?
+  helper_method :correct_user?
 
   private
     def current_user
@@ -20,18 +20,16 @@ class ApplicationController < ActionController::Base
       return true if current_user
     end
 
-    def current_user?
+    def correct_user?
       @user = User.find(params[:id])
       unless current_user == @user
-        redirect_to root_url
-        :alert => 'Access Denied'
+        redirect_to root_url, :alert => 'Access Denied'
       end
     end
 
     def authenticate_user!
       if !current_user
-        redirect_to root_url
-        :alert => 'You need to sign in for access to this page.'
+        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
       end
     end
 end
